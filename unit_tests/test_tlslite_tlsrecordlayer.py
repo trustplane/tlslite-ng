20e80fe9d1d02f7faaa4b070c3d7d6a2032f569e
Hubert Kario <hkario@redhat.com>
2015-02-14 12:05:26 +0100

    more characterisation tests for record layer

--- unit_tests/test_tlslite_tlsrecordlayer.py
+++ unit_tests/test_tlslite_tlsrecordlayer.py
@@ -11,6 +11,7 @@ except ImportError:
     from unittest.mock import call
 
 import socket
+import tlslite.tlsrecordlayer
 from tlslite.tlsrecordlayer import TLSRecordLayer
 from tlslite.messages import ClientHello, ServerHello, Certificate, \
         ServerHelloDone, ClientKeyExchange, ChangeCipherSpec, Finished, \
@@ -18,7 +19,8 @@ from tlslite.messages import ClientHello, ServerHello, Certificate, \
 from tlslite.extensions import TLSExtension
 from tlslite.constants import ContentType, HandshakeType, CipherSuite, \
         CertificateType
-from tlslite.errors import TLSLocalAlert, TLSAbruptCloseError
+from tlslite.errors import TLSLocalAlert, TLSAbruptCloseError, \
+        TLSClosedConnectionError
 from tlslite.mathtls import calcMasterSecret, PRF_1_2
 from tlslite.x509 import X509
 from tlslite.x509certchain import X509CertChain
@@ -31,6 +33,182 @@ class TestTLSRecordLayer(unittest.TestCase):
         record_layer = TLSRecordLayer(None)
 
         self.assertIsInstance(record_layer, TLSRecordLayer)
+
+    def test_read_with_empty_socket(self):
+        mock_sock = MockSocket(bytearray(0))
+
+        record_layer = TLSRecordLayer(mock_sock)
+
+        res = record_layer.read(2)
+
+        self.assertEqual(res, b'')
+
+    def test_read_with_closed_socket(self):
+        mock_sock = MockSocket(bytearray(\
+                b'\x17' +           # content type - application data
+                b'\x03\x03' +       # TLSv1.2
+                b'\x00\x04' +       # length
+                b'text'
+                ))
+
+        record_layer = TLSRecordLayer(mock_sock)
+
+        res = record_layer.read(4)
+
+        # read from "closed" socket
+        self.assertEqual(res, b'')
+
+    def test_read_with_unencrypted_data(self):
+        mock_sock = MockSocket(bytearray(\
+                b'\x17' +           # content type - application data
+                b'\x03\x03' +       # TLSv1.2
+                b'\x00\x04' +       # length
+                b'text'
+                ))
+
+        record_layer = TLSRecordLayer(mock_sock)
+        record_layer.closed = False
+
+        res = record_layer.read(4)
+
+        self.assertEqual(res, b'text')
+
+    def test_readAsync(self):
+        mock_sock = MockSocket(bytearray(\
+                b'\x17' +           # content type - application data
+                b'\x03\x03' +       # TLSv1.2
+                b'\x00\x04' +       # length
+                b'text'
+                ))
+
+        record_layer = TLSRecordLayer(mock_sock)
+        record_layer.closed = False
+
+        for result in record_layer.readAsync(4):
+            if result in (0, 1):
+                raise Exception("blocked read")
+            else: break
+
+        self.assertEqual(result, b'text')
+
+        # socket is empty so it should return "retry needed"
+        self.assertEqual(0, next(record_layer.readAsync(4)))
+
+    def test_write(self):
+
+        mock_sock = MockSocket(bytearray(0))
+
+        record_layer = TLSRecordLayer(mock_sock)
+
+        with self.assertRaises(TLSClosedConnectionError):
+            record_layer.write('text')
+
+    def test_write_with_open_socket(self):
+        mock_sock = MockSocket(bytearray(0))
+
+        record_layer = TLSRecordLayer(mock_sock)
+        record_layer.closed = False
+
+        record_layer.write(b'text')
+
+        self.assertEqual([bytearray(
+            b'\x17' +           # content type - application data
+            b'\x00\x00' +       # version - uninitialised
+            b'\x00\x04' +       # length
+            b'text')], mock_sock.sent)
+
+    def test_close(self):
+        mock_sock = MockSocket(bytearray(0))
+
+        record_layer = TLSRecordLayer(mock_sock)
+        record_layer.closed = False
+        record_layer._refCount = 1
+
+        record_layer.close()
+        self.assertEqual([bytearray(
+            b'\x15' +           # type - alert
+            b'\x00\x00' +       # version
+            b'\x00\x02' +       # length
+            b'\x01' +           # alert level - warning
+            b'\x00'             # alert description - close_notify
+            )], mock_sock.sent)
+
+    def test_getCipherName(self):
+        record_layer = TLSRecordLayer(None)
+
+        self.assertEqual(None, record_layer.getCipherName())
+
+    def test_getCipherName_with_initialised_context(self):
+        record_layer = TLSRecordLayer(None)
+        record_layer.version = (3, 0)
+
+        record_layer._calcPendingStates(
+                CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+                bytearray(48), bytearray(32), bytearray(32), None)
+
+        record_layer._changeWriteState()
+
+        self.assertEqual('aes128', record_layer.getCipherName())
+
+    def test_getCipherImplementation(self):
+        record_layer = TLSRecordLayer(None)
+
+        self.assertEqual(None, record_layer.getCipherImplementation())
+
+    def test_getCipherImplementation_with_initialised_context(self):
+        record_layer = TLSRecordLayer(None)
+        record_layer.version = (3, 0)
+
+        record_layer._calcPendingStates(
+                CipherSuite.TLS_RSA_WITH_AES_128_CBC_SHA,
+                bytearray(48), bytearray(32), bytearray(32), None)
+
+        record_layer._changeWriteState()
+
+        if tlslite.tlsrecordlayer.m2cryptoLoaded:
+            self.assertEqual('openssl', record_layer.getCipherImplementation())
+        else:
+            self.assertEqual('python', record_layer.getCipherImplementation())
+
+    def test_unread(self):
+        mock_sock = MockSocket(bytearray(\
+                b'\x17' +           # content type - application data
+                b'\x03\x03' +       # TLSv1.2
+                b'\x00\x04' +       # length
+                b'text'
+                ))
+
+        record_layer = TLSRecordLayer(mock_sock)
+        record_layer.closed = False
+
+        record_layer.unread(b'some')
+
+        self.assertEqual(b'some', record_layer.read(8))
+        self.assertEqual(b'text', record_layer.read(8))
+
+    def test_getVersionName_with_SSL3(self):
+        record_layer = TLSRecordLayer(None)
+        record_layer.version = (3, 0)
+
+        self.assertEqual('SSL 3.0', record_layer.getVersionName())
+
+    def test_getVersionName_with_TLS10(self):
+        record_layer = TLSRecordLayer(None)
+        record_layer.version = (3, 1)
+
+        self.assertEqual('TLS 1.0', record_layer.getVersionName())
+
+    def test_getVersionName_with_TLS11(self):
+        record_layer = TLSRecordLayer(None)
+        record_layer.version = (3, 2)
+
+        self.assertEqual('TLS 1.1', record_layer.getVersionName())
+
+    def test_getVersionName_with_TLS12(self):
+        record_layer = TLSRecordLayer(None)
+        record_layer.version = (3, 3)
+
+        self.assertEqual('TLS 1.2', record_layer.getVersionName())
 
     def test__sendMsg(self):
         mock_sock = mock.create_autospec(socket.socket)
