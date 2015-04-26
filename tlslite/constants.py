f418e93162ddf06576201f0e2b16090c7579e977
David Benjamin <davidben@google.com>
2015-04-25 21:43:54 -0400

    Fix client auth for TLS 1.2.
    Client auth now participates in signature algorithms. Test-wise, this
    was already covered by test 14, but since both sides implemented it
    wrong the test passed. Add a test at TLS 1.1 so coverage of the
    hash-less codepath isn't lost.
    
    From Chromium.

--- tlslite/constants.py
+++ tlslite/constants.py
@@ -47,7 +47,21 @@ class ExtensionType:    # RFC 6066 / 4366
     encrypt_then_mac = 22 # RFC 7366
     tack = 0xF300
     supports_npn = 13172
-    
+
+class HashAlgorithm:
+    none = 0
+    md5 = 1
+    sha1 = 2
+    sha224 = 3
+    sha256 = 4
+    sha384 = 5
+
+class SignatureAlgorithm:
+    anonymous = 0
+    rsa = 1
+    dsa = 2
+    ecdsa = 3
+
 class NameType:
     host_name = 0
 
